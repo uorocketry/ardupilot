bb8d7547ac84a59e06dc4930b3ef21691e014632
Jonathan Papineau <jonapap2001@gmail.com>
2020-01-08 14:33:24 -0500

    Disabled check preventing to switch to auto if first cmd is not takeoff

--- ArduCopter/mode_auto.cpp
+++ ArduCopter/mode_auto.cpp
@@ -25,11 +25,13 @@ bool Copter::ModeAuto::init(bool ignore_checks)
     if ((copter.position_ok() && copter.mission.num_commands() > 1) || ignore_checks) {
         _mode = Auto_Loiter;
 
+        /*
         // reject switching to auto mode if landed with motors armed but first command is not a takeoff (reduce chance of flips)
         if (motors->armed() && ap.land_complete && !copter.mission.starts_with_takeoff_cmd()) {
             gcs().send_text(MAV_SEVERITY_CRITICAL, "Auto: Missing Takeoff Cmd");
             return false;
         }
+        */
 
         // stop ROI from carrying over from previous runs of the mission
         // To-Do: reset the yaw as part of auto_wp_start when the previous command was not a wp command to remove the need for this special ROI check
